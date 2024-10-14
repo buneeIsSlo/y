23180e32db10e10f66b35bc4c3d66e2223fc0a2c
bunee <buneeisslo@gmail.com>
2024-10-14 23:08:41 +0530

    Feat: implement signup, login, and logout server actions

---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,82 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { lucia } from "@/auth";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password, email } = signUpSchema.parse(credentials);
+    const passwordHash = await hash(password, {
+      /* Recommended minimum parameters. Reason -> https://thecopenhagenbook.com/password-authentication#password-storage */
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+    const userId = generateIdFromEntropySize(10); // 16 characters long
+
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return {
+        error: "Username's already taken",
+      };
+    }
+
+    const exisistingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (exisistingEmail) {
+      return {
+        error: "Email alread exists",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email: email,
+        passwordHash,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = await lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Oops, something went wrong. Please try again!",
+    };
+  }
+}
