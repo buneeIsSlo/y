5bc9d014bdf01d6f49550734ce179308ffa21e31
bunee <buneeisslo@gmail.com>
2024-10-29 17:12:31 +0530

    Feat: Load posts on home page along with time of creation

--- src/app/(core)/page.tsx
+++ src/app/(core)/page.tsx
@@ -1,10 +1,20 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
   return (
-    <div className="">
-      <div className="min-h-[300dvh] w-full">
+    <div className="w-full min-w-0">
+      <div className="min-h-[300dvh] w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,43 @@
+import { PostData } from "@/lib/types";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { formatRelativeDate } from "@/lib/utils";
+
+interface PostProps {
+  post: PostData;
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <article className="space-y-3 rounded-2xl border bg-card p-5 shadow-sm">
+      <div className="flex flex-wrap gap-3">
+        <Link href={`/users/${post.user.username}`}>
+          <Avatar className="">
+            <AvatarImage
+              src={post.user.avatarUrl}
+              alt={`@${post.user.username}`}
+            />
+            <AvatarFallback className="capitalize">
+              {post.user.displayName.slice(0, 2)}
+            </AvatarFallback>
+          </Avatar>
+        </Link>
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link
+            href={`/post/${post.id}`}
+            className="block text-sm text-muted-foreground hover:underline"
+          >
+            {formatRelativeDate(post.createdAt)}
+          </Link>
+        </div>
+      </div>
+      <div className="whitespace-pre-line break-words">{post.content}</div>
+    </article>
+  );
+}
