4177b14d026844c48e381d343dcedd36d2d68688
bunee <buneeisslo@gmail.com>
2024-12-05 23:39:40 +0530

    Add: uploadthing and create filerouter(draft)

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig = {
       dynamic: 30,
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
